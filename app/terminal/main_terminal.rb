4a6f3dc753616096d2bbcae239db863498ff2969
Brad Gessler <bradgessler@gmail.com>
2025-02-27 15:39:40 -0800

    Add domains feature

---
+++ app/terminal/main_terminal.rb
@@ -0,0 +1,42 @@
+class MainTerminal < ApplicationTerminal
+  desc "domains", "List all domains"
+  def domains
+    Domain.all.each do |domain|
+      puts domain.name
+    end
+  end
+
+  desc "login", "Login to your account"
+  def login
+    print "Email: "
+    email = gets.chomp
+
+    print "Password: "
+    password = getpass
+
+    # Replace this with your own authentication logic; this is an example
+    # of how you might do this with Devise.
+    user = User.find_for_authentication(email: email)
+    if user && user.valid_password?(password)
+      self.current_user = user
+      puts "Successfully logged in as #{current_user.email}."
+    else
+      fail "Could not find a user with that email and password."
+    end
+  end
+
+  desc "whoami", "Displays current user information."
+  def whoami
+    if self.current_user
+      puts "Logged in as #{current_user.email}."
+    else
+      fail "Not logged in. Run `#{self.class.basename} login` to login."
+    end
+  end
+
+  desc "logout", "Logout of your account"
+  def logout
+    session.reset
+    puts "Successfully logged out."
+  end
+end
